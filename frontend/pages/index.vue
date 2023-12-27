1275740db06abc8632ffe72497ea2fbf5a995f16
TDarkDev <azizbek@talentum.id>
2023-12-27 20:02:11 +0500

    FRM-14: Generate different link for Q&A table

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -111,7 +111,9 @@ const requestsRows = computed(
       link: {
         component: Link,
         props: {
-          text: 'https://www.figma.com/',
+          text: `https://formify.io/qa-question-${Math.floor(
+            Math.random() * (99999 - 10000 + 1) + 10000,
+          )}}`,
           value: '',
         },
       },

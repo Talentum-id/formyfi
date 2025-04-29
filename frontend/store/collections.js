cc8e72811e1e9766bf4e15b2ddbcab4408b89cad
Azizbek Khalilov <azizbek@talentum.id>
2025-04-29 23:04:44 +0500

    Fix nft creation

--- frontend/store/collections.js
+++ frontend/store/collections.js
@@ -66,7 +66,10 @@ export const useCollectionsStore = defineStore('collections', {
       this.identity = identity;
     },
     async createCollection(params) {
-      params.available = params.max_supply;
+      params = {
+        ...params,
+        available: params.max_supply,
+      };
 
       return await this.actor?.createCollection(params, {
         identity: process.env.DFX_ASSET_PRINCIPAL,
